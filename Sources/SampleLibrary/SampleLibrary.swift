d28735ab2e7c44ba044d90f0310d5bb872ae1d0e
Bravothe <Bravo@evzone.app>
2025-03-26 18:07:04 +0300

    Init statement

--- Sources/SampleLibrary/SampleLibrary.swift
+++ Sources/SampleLibrary/SampleLibrary.swift
@@ -1,6 +1,10 @@
 public struct SampleLibrary {
    var text = "Hello i am Allan"
     
+    public init(){
+        
+    }
+    
     public func HelloToYou() -> String{
         return "Hello to You"
     }
