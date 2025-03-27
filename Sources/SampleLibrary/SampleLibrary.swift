80cfccd559af2a80169c3329fc730af67f94471e
Bravothe <Bravo@evzone.app>
2025-03-27 17:03:12 +0300

    updated sdk with the popup

--- Sources/SampleLibrary/SampleLibrary.swift
+++ Sources/SampleLibrary/SampleLibrary.swift
@@ -1,11 +1,22 @@
+import Foundation
+
+// Protocol for showing the popup
+public protocol PopupPresentable {
+    func showPopup(with message: String)
+}
+
 public struct SampleLibrary {
-   var text = "Hello i am Allan yyyy"
+    var text = "Hello I am Allan Thembo Ndungu"
     
-    public init(){
-        
+    public init() {}
+    
+    public func HelloToYou() -> String {
+        return "Hello to You"
     }
     
-    public func HelloToYou() -> String{
-        return "Hello to You"
+    // Function to combine the messages and trigger the popup
+    public func showMessage(delegate: PopupPresentable) {
+        let combinedMessage = "\(text) \(HelloToYou())"
+        delegate.showPopup(with: combinedMessage)
     }
 }
